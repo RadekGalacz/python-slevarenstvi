0968d2459445f15f44f4d35a008c078f0dab969d
Radek Galacz <rgalacz@gmail.com>
2024-06-20 19:11:26 +0200

    Přidán obrázek odlitku + úprava rozmístění Labelu

--- grafika.py
+++ grafika.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageTk
 from plocha_zarezu import F_zarezu
 
 root = Tk()
-root.title('Výpočet vtokových soustav')
+root.title('Výpočet vtokových soustav litinových odlitků')
 root.geometry('900x500')
 root.resizable(False, False)
 
@@ -29,7 +29,7 @@ entry_LK = Entry(root, width=4, justify=CENTER)
 entry_LK.grid(row=1, column=3)
 
 # Label - text, zadej poměr
-label_pomer = Label(root, text='Zadej poměr: ')
+label_pomer = Label(root, text='Zadej poměr: ', anchor='w', width=22)
 label_pomer.grid(row=1, column=0)
 
 def pomer_vtokovky():
@@ -48,7 +48,7 @@ button.grid(row=1, column=4)
 
 # Label - vypsání poměru vtokové soustavy: podtlak/přetlak
 label_vysledek = Label(root, text = '')
-label_vysledek.grid(row=1, column=5)
+label_vysledek.place(x=630, y=40)
 
 # Label - text, zadej hmotnot odlitku
 label_pomer = Label(root, text='Zadej hmotnost odlitku (kg): ')
@@ -87,11 +87,44 @@ image = open_image.resize((265, 139))
 image = ImageTk.PhotoImage(image)
 
 image_label = Label(root, image=image)
-image_label.grid(row=2, column=6)
+image_label.place(x=350, y=0)  
 
 # Label - vypsání hmotnosti - plochy zářezů
 label_plocha= Label(root, text = '')
-label_plocha.grid(row=2, column=5)
+label_plocha.place(x=630, y=60)
+
+# H - výška hladiny nad zářezem
+label_h = Label(root, text='h (mm)')
+label_h.place(x=170, y=180)
+
+entry_h = Entry(root, width=6, justify=CENTER)
+entry_h.place(x=172, y=200)
+
+# A - výška odlitku nad zářezem
+label_a = Label(root, text='a (mm)')
+label_a.place(x=220, y=180)
+
+entry_a = Entry(root, width=6, justify=CENTER)
+entry_a.place(x=222, y=200)
+
+# C - celková výška odlitku
+label_c = Label(root, text='c (mm)')
+label_c.place(x=270, y=180)
+
+entry_c = Entry(root, width=6, justify=CENTER)
+entry_c.place(x=272, y=200)
+
+# Label - text, zadej hodnoty podle obrázku
+label_pomer = Label(root, text='Zadej hodnoty podle obrázku: ')
+label_pomer.place(x=0, y=200)
+
+# Obrázek odlitku
+open_image = Image.open('odlit.png')
+image1 = open_image.resize((265, 139))
+image1 = ImageTk.PhotoImage(image1)
+
+image1_label = Label(root, image=image1)
+image1_label.place(x=350, y=150)  
 
 
 root.mainloop()
